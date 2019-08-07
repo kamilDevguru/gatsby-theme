833dacfede4e7c51fee45b3a2eafe6ea4f0d0635
kamilDevguru <kamil.czoska1997@outlook.com>
2019-08-07 22:19:33 +0200

    Integrate with contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `A react next landing page`,
@@ -82,6 +86,14 @@ module.exports = {
             variants: [`300`, `400`, `600`, `700`, `800`],
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `qhk0encle6so`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
